e3ce2b9ead68bd41d970ca6bbc622c38f95cb882
steven <8312553+sphred5@users.noreply.github.com>
2022-07-09 12:20:24 -0700

    added front end code to change password form component:

--- components/profile/profile-form.js
+++ components/profile/profile-form.js
@@ -1,15 +1,31 @@
-import classes from './profile-form.module.css';
+import { useRef } from "react";
+import classes from "./profile-form.module.css";
 
-function ProfileForm() {
+function ProfileForm(props) {
+  const newPasswordInputRef = useRef();
+  const oldPasswordInputRef = useRef();
+
+  function submitHandler(e) {
+    e.preventDefault();
+
+    enteredOldPassword = oldPasswordInputRef.current.value;
+    enteredNewPassword = newPasswordInputRef.current.value;
+
+    // TODO add validation
+    props.onChangePassword({
+      oldPassword: enteredOldPassword,
+      newPassword: enteredNewPassword,
+    });
+  }
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <div className={classes.control}>
-        <label htmlFor='new-password'>New Password</label>
-        <input type='password' id='new-password' />
+        <label htmlFor="new-password">New Password</label>
+        <input type="password" id="new-password" ref={newPasswordInputRef} />
       </div>
       <div className={classes.control}>
-        <label htmlFor='old-password'>Old Password</label>
-        <input type='password' id='old-password' />
+        <label htmlFor="old-password">Old Password</label>
+        <input type="password" id="old-password" ref={oldPasswordInputRef} />
       </div>
       <div className={classes.action}>
         <button>Change Password</button>
